4b8cf7b497750d678d93b710e68c22870ef5615e
shakedva <shakediii123@gmail.com>
2023-03-21 20:26:02 +0200

    added basic functionality to vector class

--- vector/include/Vector.h
+++ vector/include/Vector.h
@@ -1,10 +1,19 @@
 #pragma once
+#include <iostream>
 
 class Vector
 {
 public:
 	Vector(size_t size = 0, int value = 0);
-	Vector(const Vector&);
+	Vector(const Vector&); // copy c-tor
+	~Vector();
+	Vector& operator=(const Vector&);
+	size_t size();
+	size_t capacity();
+	bool empty();
+	int* data();
+	void clear();
+	void print();
 private:
 	size_t _size;
 	size_t _capacity;
--- vector/src/Vector.cpp
+++ vector/src/Vector.cpp
@@ -2,9 +2,6 @@
 
 Vector::Vector(size_t size, int value) : _size(size), _capacity(size), _data(new int[size])
 {
-	//_size = size;
-	//_capacity = size;
-	//_data = new int[size];
 	for (int i = 0; i < _size; i++)
 		_data[i] = value;
 }
@@ -14,7 +11,59 @@ Vector::Vector(const Vector& other)
 	_size = other._size;
 	_capacity = other._capacity;
 	_data = new int[_size];
-	for (int i = 0; i < _capacity; i++)
+	for (int i = 0; i < _size; i++)
 		_data[i] = other._data[i];
 }
+
+Vector::~Vector()
+{
+	delete[] _data;
+}
+
+Vector& Vector::operator=(const Vector &other)
+{
+	if (this == &other)
+		return *this;
+
+	delete[] _data;
+	_data = new int[other._capacity];
+	_size = other._size;
+	_capacity = other._capacity;
+	for (int i = 0; i < _size; i++)
+		_data[i] = other._data[i];
+	return *this;
+}
+
+void Vector::print()
+{
+	for (int i = 0; i < _size; i++)
+		std::cout << _data[i];
+}
+
+size_t Vector::size()
+{
+	return _size;
+}
+
+size_t Vector::capacity()
+{
+	return _capacity;
+}
+
+bool Vector::empty()
+{
+	return _size == 0;
+}
+
+int* Vector::data()
+{
+	return _size == 0 ? nullptr : _data;
+}
+
+void Vector::clear()
+{
+	delete[] _data;
+	_data = nullptr;
+	_size = 0;
+}	
 

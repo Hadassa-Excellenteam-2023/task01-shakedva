f9774bf4cc482fd37df7187f7921f6742a2489a6
shakedva <shakediii123@gmail.com>
2023-03-23 14:49:10 +0200

    finished

--- vector/include/Vector.h
+++ vector/include/Vector.h
@@ -6,10 +6,8 @@ class Vector
 {
 public:
 	Vector(size_t size = 0, int value = 0);
-	// copy ctor and assignment 
 	Vector(const Vector&);
 	Vector& operator=(const Vector&);
-	// move ctor and assignment 
 	Vector(Vector&&);
 	Vector& operator=(Vector&&);
 	~Vector();
@@ -18,14 +16,12 @@ public:
 	bool empty();
 	int* data();
 	void clear();
-	void print();
 	void swap(Vector&);
 	void resize(size_t);
 	void push_back(const int&);
 	void pop_back();
 	void erase(size_t);
 	void insert(size_t, const int&);
-	
 	int& operator[](size_t);
 	int operator[](size_t) const;
 	bool operator>(const Vector&);
@@ -43,3 +39,4 @@ private:
 
 bool operator==(const Vector&, const Vector&);
 bool operator!=(const Vector&, const Vector&);
+std::ostream& operator<<(std::ostream&, const Vector&);
--- vector/src/Vector.cpp
+++ vector/src/Vector.cpp
@@ -1,26 +1,36 @@
 #include "Vector.h"
 #include <algorithm>
-
+/*
+* Constructor that receives optional size and value. 
+* Initialize a container with the size and fill it with the value. 
+*/
 Vector::Vector(size_t size, int value) : _size(size), _capacity(size), _data(new int[size])
 {
 	for (int i = 0; i < _size; i++)
 		_data[i] = value;
 }
-
+/*
+* Copy constructor that receives a Vector.
+* Deep copy all the Vector's data.
+*/
 Vector::Vector(const Vector& other)
 {
 	_size = other._size;
 	_capacity = other._capacity;
 	_data = new int[_size];
-	for (int i = 0; i < _size; i++)
+	for (size_t i = 0; i < _size; i++)
 		_data[i] = other._data[i];
 }
-
+/*
+* Destructor.
+*/
 Vector::~Vector()
 {
 	delete[] _data;
 }
-
+/*
+* Copy assignment operator overload that receives a Vector and copy it's data. 
+*/
 Vector& Vector::operator=(const Vector &other)
 {
 	if (this == &other)
@@ -34,91 +44,109 @@ Vector& Vector::operator=(const Vector &other)
 		_data[i] = other._data[i];
 	return *this;
 }
-
+/*
+* Move constructor that receives a lvalue Vector.
+*/
 Vector::Vector(Vector&& other) 
 	: _size(other._size), _capacity(other._capacity), _data(other._data)
 {
-	std::cout << "in move ctor\n";
 	other._size = 0;
 	other._capacity = 0;
 	other._data = nullptr;
 }
-
+/*
+* Move assignment operator overload that receives a lvalue Vector.
+*/
 Vector& Vector::operator=(Vector&& other)
 {
-	std::cout << "in move assignment\n";
 	if (this != &other)
 	{
 		delete[] _data;
 		_data = other._data;
 		_size = other._size;
 		_capacity = other._capacity;
-
+		
 		other._size = 0;
 		other._capacity = 0;
 		other._data = nullptr;
 	}
 	return *this;
 }
-
+/*
+* Access specified element
+*/
 int& Vector::operator[](size_t index)
 {
-	if (isInRange(index)) 
-		return _data[index];	
+	if (!isInRange(index))
+		throw std::out_of_range("Index out range");
+	return _data[index];
 }
-
+/*
+* Access specified element
+*/
 int Vector::operator[](size_t index) const
 {
-	if (isInRange(index)) 
-		return _data[index];
+	if (!isInRange(index))
+		throw std::out_of_range("Index out range");
+	return _data[index];
 }
 
 bool Vector::isInRange(size_t index) const
 {
 	return index < _size;
 }
-
+/*
+* Calls resize with a specific capacity, depends on the current vector's capacity. 
+*/
 void Vector::addCapacity()
 {
-	_capacity = (_capacity == 0) ? 1 : _capacity;
-	double multiply = _capacity > 128 ? 1.5 : 2;
-	size_t newCapacity = (size_t)(_capacity * multiply);
+	int capacity = (_capacity == 0) ? 1 : _capacity;
+	double multiply = capacity > 128 ? 1.5 : 2;
+	size_t newCapacity = (size_t)(capacity * multiply);
 	resize(newCapacity);
 }
-
-void Vector::print()
-{
-	//std::cout << "size: " << _size << " capacity: " << _capacity << '\n';
-	for (int i = 0; i < _size; i++)
-		std::cout << _data[i] << " ";
-	std::cout << std::endl;
-}
-
+/*
+* Receives two Vectors and swaps their data. 
+*/
 void Vector::swap(Vector& other)
 {
 	Vector temp = other;
 	other = *this;
 	*this = temp;	
 }
-
+/*
+* Receive capacity and resizes the _data accordingly.
+* If the current capacity is biggen then received, the container will be reduced. 
+* Otherwise, the container will add additional 0s at the end. 
+*/
 void Vector::resize(size_t capacity)
 {
-	if (capacity == _capacity)
+	if (capacity == _capacity) // no need to resize
 		return;
-
 	int* temp = new int[capacity];
-	for (int i = 0; i < capacity; i++)
-		if (i < _capacity)
-			temp[i] = _data[i];
-		else
-			temp[i] = 0;
+	if (capacity != 0)
+	{
+		temp = new int[capacity];
+		for (size_t i = 0; i < capacity; i++)
+		{
+			// when capacity is 0, it has nothing to add from _data to temp. 
+			// add additional 0s to the end. 
+ 			if (i < _capacity && _capacity > 0)
+				temp[i] = _data[i];
+			else
+				temp[i] = 0;
+		}
+	}
 	delete[] _data;
 	_data = temp;
+	// if received smaller capacity, some values were truncated.
 	_size = capacity < _capacity ? capacity : _size;
 	_capacity = capacity;
 	
 }
-
+/*
+* Adds an element to the end, and resizes if needed.
+*/
 void Vector::push_back(const int& value)
 {
 	if (_size >= _capacity)
@@ -127,16 +155,23 @@ void Vector::push_back(const int& value)
 	_data[_size] = value;
 	_size++;
 }     
-
+/*
+* Removes the last element
+*/
 void Vector::pop_back()
 {
-	_size--;
-	_data[_size] = 0;
+	if (_size > 0) {
+		_size--;
+		_data[_size] = 0;
+	}
 }
-
+/*
+* Erases the element in the index received
+*/
 void Vector::erase(size_t index)
 {
-	if (index > _size) {
+	
+	if (!isInRange(index)) {
 		std::cout << "Index out of range" << std::endl;
 		return;
 	}
@@ -144,16 +179,22 @@ void Vector::erase(size_t index)
 		_data[i] = _data[i + 1];
 	_size--;
 }
-
+/*
+* Inserts an element in the index received
+*/
 void Vector::insert(size_t index, const int& value)
 {
+	if (!isInRange(index)) {
+		std::cout << "Index out of range" << std::endl;
+		return;
+	}
 	if (index == _size)
 		push_back(value);
-	else
+	else 
 	{
 		if (_size >= _capacity)
 			addCapacity();
-
+		
 		for (int i = _size; i > index; i--)
 			_data[i] = _data[i - 1];
 		_data[index] = value;
@@ -188,7 +229,10 @@ void Vector::clear()
 	_size = 0;
 }	
 
-// lexicographically greater than the contents of other
+/*
+* Checks if _data is lexicographically greater than the other Vector by comparing
+* their values. 
+*/
 bool Vector::operator>(const Vector& other)
 {
 	size_t i = 0;
@@ -202,22 +246,30 @@ bool Vector::operator>(const Vector& other)
 	}
 	return _size > other._size;
 }
-
+/*
+* Checks if _data is lexicographically greater or equal than the other Vector.
+*/
 bool Vector::operator>=(const Vector& other)
 {
 	return *this > other || *this == other ;
 }
-
+/*
+* Checks if _data is lexicographically lesser than the other Vector.
+*/
 bool Vector::operator<(const Vector& other)
 {
 	return !(*this >= other);
 }
-
+/*
+* Checks if _data is lexicographically lesser or equal  than the other Vector.
+*/
 bool Vector::operator<=(const Vector& other)
 {
 	return *this < other || *this == other;
 }
-
+/*
+* Checks if _data is lexicographically equal to the other Vector.
+*/
 bool operator==(const Vector& l, const Vector& r)
 {
 	if (l.size() != r.size())
@@ -231,8 +283,19 @@ bool operator==(const Vector& l, const Vector& r)
 	}
 	return true;
 }
-
+/*
+* Checks if _data is lexicographically inequal to the other Vector.
+*/
 bool operator!=(const Vector& l, const Vector& r)
 {
 	return !(l == r);
+}
+/*
+* Print the vector to the ostream
+*/
+std::ostream& operator<<(std::ostream& os, const Vector& v)
+{
+	for (size_t i = 0; i < v.size(); i++)
+		os << v[i] << " ";
+	return os;
 }
